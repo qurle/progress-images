59502f90bd8959b3c78b5c6da0495787c6feca7c
Nick Qurle <wrl.snk@gmail.com>
2022-07-12 19:53:07 +0300

    i just made it

---
+++ progress.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from tkinter import filedialog as fd
+import os
+
+FORMATS = (
+    '.png', 
+    '.jpg', 
+    '.jpeg',
+    '.bmp',
+    '.gif',
+    '.tiff'
+)
+dir = fd.askdirectory()
+
+for name in os.listdir(dir):
+    f = os.path.join(dir, name).replace("\\","/")
+    # checking if it is a file
+    if f.lower().endswith(FORMATS):
+        input = Image.open(f)
+        input = input.convert('RGB')
+        output = os.path.splitext(f)[0]+'.jpg'
+        input.save(output, "JPEG",  optimize=True, quality=90, progressive=True)
